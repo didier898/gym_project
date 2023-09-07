b46c452efc819b3a9d6eaaf3773cebe5728c5b75
Didier <didier002010@gmail.com>
2023-09-07 12:54:00 -0500

    configuracion inicial

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Gym from './complements/Gym'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Gym />
+  </React.StrictMode>,
+)
